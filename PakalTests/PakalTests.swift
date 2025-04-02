36661a3e6dc5881cb40bbdbcc501e9dc24093764
Antonio Medina Montoya <antoniomedina0619@gmail.com>
2025-04-02 11:06:34 -0600

    Inicializacion

---
+++ PakalTests/PakalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PakalTests.swift
+//  PakalTests
+//
+//  Created by Antonio Medina on 02/04/25.
+//
+
+import Testing
+@testable import Pakal
+
+struct PakalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
